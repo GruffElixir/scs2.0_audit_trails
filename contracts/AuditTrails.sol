91397699c317b8dc09f422d9ffff413cf216c558
0shawott <devis00005@gmail.com>
2024-10-27 00:45:11 +0530

    Multiple Peer support(Single peer features removed**)
    Figure out how to use approval Block, single peer features removed after move to multiple peers. More functionality need to be added.
    Also dploy.js modified

--- contracts/AuditTrails.sol
+++ contracts/AuditTrails.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.27; // Specify the compiler version
 
 contract AuditTrail {
     struct Record {
         string sender;
         string receiver;
         uint256 amount;
-        string purpose;  // Added purpose field
+        string purpose;
     }
 
     struct Block {
@@ -19,11 +19,17 @@ contract AuditTrail {
     }
 
     Block[] public chain;
+    address[3] public approvers; // Array to store the 3 trusted approvers' addresses
     uint256 public difficulty = 1;
+
+    mapping(uint256 => mapping(address => bool)) public approvals; // Track approvals per block by each approver
+    mapping(uint256 => uint8) public approvalCount; // Count approvals for each block
 
     event BlockAdded(uint256 index, bytes32 blockHash, bytes32 prevHash);
 
-    constructor() {
+    constructor(address[3] memory _approvers) {
+        approvers = _approvers;
+
         bytes32 genesisHash = keccak256(abi.encodePacked("Genesis"));
         chain.push(Block({
             record: Record("Genesis", "Genesis", 0, "Genesis Block"),
@@ -33,6 +39,18 @@ contract AuditTrail {
             nonce: 0,
             blockHash: genesisHash
         }));
+    }
+
+    modifier onlyApprover() {
+        require(isApprover(msg.sender), "Not an authorized approver");
+        _;
+    }
+
+    function isApprover(address _addr) internal view returns (bool) {
+        for (uint i = 0; i < approvers.length; i++) {
+            if (approvers[i] == _addr) return true;
+        }
+        return false;
     }
 
     function calculateHash(
@@ -52,16 +70,21 @@ contract AuditTrail {
         return uint8(uint256(hash) & 0xFF) < 256 - (difficulty * 16);
     }
 
-    function addBlock(string memory sender, string memory receiver, uint256 amount, string memory purpose) public {
+    function proposeBlock(
+        string memory sender,
+        string memory receiver,
+        uint256 amount,
+        string memory purpose
+    ) public onlyApprover returns (uint256) {
         Block memory prevBlock = chain[chain.length - 1];
-        
+
         uint256 nonce = 0;
         bytes32 hash;
         uint256 timestamp = block.timestamp;
-        
+
         uint256 maxIterations = 1000;
         bool found = false;
-        
+
         for (uint256 i = 0; i < maxIterations; i++) {
             hash = calculateHash(sender, receiver, amount, purpose, msg.sender, prevBlock.blockHash, timestamp, nonce);
             if (proofOfWork(hash)) {
@@ -70,7 +93,7 @@ contract AuditTrail {
             }
             nonce++;
         }
-        
+
         require(found, "Failed to find valid nonce within gas limits");
 
         Block memory newBlock = Block({
@@ -82,8 +105,21 @@ contract AuditTrail {
             blockHash: hash
         });
 
-        chain.push(newBlock);
-        emit BlockAdded(chain.length - 1, hash, prevBlock.blockHash);
+        chain.push(newBlock); // Add block with no finalization yet
+        return chain.length - 1; // Return index of the newly created block
+    }
+
+    function approveBlock(uint256 blockIndex) public onlyApprover {
+        require(blockIndex < chain.length, "Invalid block index");
+        require(!approvals[blockIndex][msg.sender], "Already approved by this approver");
+
+        approvals[blockIndex][msg.sender] = true; // Record approver’s approval
+        approvalCount[blockIndex] += 1;
+
+        // Finalize the block only after all 3 approvals
+        if (approvalCount[blockIndex] == 3) {
+            emit BlockAdded(blockIndex, chain[blockIndex].blockHash, chain[blockIndex].prevHash);
+        }
     }
 
     function isValid() public view returns (bool) {
@@ -105,22 +141,12 @@ contract AuditTrail {
                 currentBlock.timestamp,
                 currentBlock.nonce
             );
-            
+
             if (recalculatedHash != currentBlock.blockHash) {
                 return false;
             }
         }
         return true;
-    }
-
-    function calculateSummaries() public view returns (uint256 totalTransactions, uint256 totalVolume, uint256 avgTransactionValue) {
-        totalTransactions = chain.length - 1; // Subtract genesis block
-        for (uint256 i = 1; i < chain.length; i++) {
-            totalVolume += chain[i].record.amount;
-        }
-        if (totalTransactions > 0) {
-            avgTransactionValue = totalVolume / totalTransactions;
-        }
     }
 
     function setDifficulty(uint256 _difficulty) public {
